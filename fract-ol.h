037ad65a5423dc9de1e33bc4c875e793ebf8d89d
fd2 <nu-fd2@users.noreply.github.com>
2025-03-03 08:47:54 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- fract-ol.h
+++ fract-ol.h
@@ -6,17 +6,22 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 21:24:36 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/03/02 21:35:05 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/03/03 00:06:52 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACT_OL_H
 #define FRACT_OL_H
 
+#include <math.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #include "ft_printf/src/ft_printf.h"
 #include "minilibx_mms_20191025_beta/mlx.h"
 #include "minilibx_opengl_20191021/mlx.h"
-#include "minilibx-linux/mlx.h"
+// #include "minilibx-linux/mlx.h"
 
 
 
